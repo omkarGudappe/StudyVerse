4b8593659cec034bdaa79a3501389ece801324d8
omkarGudappe <omkar.gudappe205@gmail.com>
2025-09-08 23:01:24 +0530

    Auth Changes

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ const io = new Server(server, {
 require('./SocketConnection/Socket')(io);
 
 app.use(cors({
-  origin: 'https://study-verse-rose.vercel.app',
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
