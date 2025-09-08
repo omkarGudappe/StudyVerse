6115de6a1884129ed360c29ea5c35f3ea666c1f3
omkarGudappe <omkar.gudappe205@gmail.com>
2025-09-08 20:00:31 +0530

    Deploy URL added

--- edusocial/src/SocketConnection/Socket.js
+++ edusocial/src/SocketConnection/Socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io("http://localhost:4000" ,{
+const socket = io("https://studyverse-megv.onrender.com",{
     withCredentials: true,
     transports: ['websocket' ],
     reconnection: true,
