e6cda9280b7c8bcf49cc230d1d8f9f1e3184bf02
omkarGudappe <omkar.gudappe205@gmail.com>
2025-09-08 23:50:17 +0530

    Error handling2

--- server/Db/connectdb.js
+++ server/Db/connectdb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.MONGODB_URL, {
+        await mongoose.connect(process.env.MONGODB_ATLAS_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
