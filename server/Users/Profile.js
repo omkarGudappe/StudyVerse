dcef0ce6eb7ba290f8ed7e2c34fe0932633e98e4
omkarGudappe <omkar.gudappe205@gmail.com>
2025-09-09 18:47:26 +0530

    New Update1

--- server/Users/Profile.js
+++ server/Users/Profile.js
@@ -131,64 +131,6 @@ Router.get('/profile/:FUid' , async (req , res) => {
     }
 })
 
-Router.post('/profile/update/:userId', async (req, res) => {
-    const { userId } = req.params;
-    // const { firstName, lastName, description, heading, gender, dob, education } = req.body;
-
-    if (!userId) {
-        return res.status(400).json({ message: "User ID is required", code: "MISSING_USER_ID" });
-    }else {
-        return res.json({exist: "Got a UserId"});
-    }
-
-
-    // try {
-    //     let updateData = {
-    //         firstName,
-    //         lastName,
-    //         dob,
-    //         gender,
-    //         education,
-    //         "UserProfile.description": description,
-    //         "UserProfile.heading": heading,
-    //     };
-
-    //     if (req.file) {
-    //         const uploadResult = await cloudinary.uploader.upload(req.file.path, {
-    //             folder: "studyverse/profiles",
-    //         });
-
-    //         updateData["UserProfile.avatar"] = {
-    //             url: uploadResult.secure_url,
-    //             publicId: uploadResult.public_id,
-    //         };
-    //     }
-
-    //     const updatedProfile = await User.findOneAndUpdate(
-    //         { firebaseUid: userId },
-    //         updateData,
-    //         { new: true }
-    //     );
-
-    //     if (!updatedProfile) {
-    //         return res.status(404).json({ message: "User not found", code: "USER_NOT_FOUND" });
-    //     }
-
-    //     res.json({ 
-    //         ok: true, 
-    //         message: "Profile updated successfully", 
-    //         user: updatedProfile 
-    //     });
-
-    // } catch (err) {
-    //     console.error("Error updating profile:", err);
-    //     res.status(500).json({ 
-    //         ok: false,
-    //         message: "Internal server error", 
-    //         code: "INTERNAL_SERVER_ERROR" 
-    //     });
-    // }
-});
 
 Router.post('/posts/:Fid', upload.single('image'), async (req, res) => {
     try{
@@ -290,5 +232,64 @@ Router.get('/:Uid/notifications', async (req, res) => {
   }
 });
 
+Router.post('/profile/update/:userId', async (req, res) => {
+    const { userId } = req.params;
+    // const { firstName, lastName, description, heading, gender, dob, education } = req.body;
+    const { firstName } = req.body;
+
+    if (!userId) {
+        return res.status(400).json({ message: "User ID is required", code: "MISSING_USER_ID" });
+    }else {
+        return res.json({exist: `Got a UserId ${firstName}`});
+    }
+
+
+    // try {
+    //     let updateData = {
+    //         firstName,
+    //         lastName,
+    //         dob,
+    //         gender,
+    //         education,
+    //         "UserProfile.description": description,
+    //         "UserProfile.heading": heading,
+    //     };
+
+    //     if (req.file) {
+    //         const uploadResult = await cloudinary.uploader.upload(req.file.path, {
+    //             folder: "studyverse/profiles",
+    //         });
+
+    //         updateData["UserProfile.avatar"] = {
+    //             url: uploadResult.secure_url,
+    //             publicId: uploadResult.public_id,
+    //         };
+    //     }
+
+    //     const updatedProfile = await User.findOneAndUpdate(
+    //         { firebaseUid: userId },
+    //         updateData,
+    //         { new: true }
+    //     );
+
+    //     if (!updatedProfile) {
+    //         return res.status(404).json({ message: "User not found", code: "USER_NOT_FOUND" });
+    //     }
+
+    //     res.json({ 
+    //         ok: true, 
+    //         message: "Profile updated successfully", 
+    //         user: updatedProfile 
+    //     });
+
+    // } catch (err) {
+    //     console.error("Error updating profile:", err);
+    //     res.status(500).json({ 
+    //         ok: false,
+    //         message: "Internal server error", 
+    //         code: "INTERNAL_SERVER_ERROR" 
+    //     });
+    // }
+});
 
 module.exports = Router;
