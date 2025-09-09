b5670eeeec52cb0736997793561d049d0da65f89
omkarGudappe <omkar.gudappe205@gmail.com>
2025-09-09 17:23:44 +0530

    New Update

--- edusocial/src/Main/RouteMenuComponent/Setting.jsx
+++ edusocial/src/Main/RouteMenuComponent/Setting.jsx
@@ -111,10 +111,7 @@ const Setting = () => {
             }
             formDataToSend.append("education", educationString);
 
-            const response = await axios.put(
-                `${import.meta.env.VITE_API_URL}/user/profile/update/${userId}`,
-                formDataToSend,
-                {
+            const response = await axios.put(`${import.meta.env.VITE_API_URL}/user/profile/update/${userId}`,formDataToSend,{
                     headers: {
                         'Content-Type': 'multipart/form-data'
                     }
