7aa3d94481d671114ee5ed4e636e3f21b0795c22
omkarGudappe <omkar.gudappe205@gmail.com>
2025-09-09 18:47:26 +0530

    New Update1

--- edusocial/src/Main/RouteMenuComponent/Setting.jsx
+++ edusocial/src/Main/RouteMenuComponent/Setting.jsx
@@ -113,7 +113,7 @@ const Setting = () => {
 
             console.log(`${import.meta.env.VITE_API_URL}/user/profile/update/${userId}`, formDataToSend);
 
-            const response = await axios.post(`${import.meta.env.VITE_API_URL}/user/profile/update/${userId}`);
+            const response = await axios.post(`${import.meta.env.VITE_API_URL}/user/profile/update/${userId}`, firstName);
 
             console.log(response);
 
