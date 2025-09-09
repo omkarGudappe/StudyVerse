ff848ca18f858a7fd0036d3d627c550af1b8ed7b
omkarGudappe <omkar.gudappe205@gmail.com>
2025-09-09 19:57:26 +0530

    New Updat3

--- server/Users/OtherUser.js
+++ server/Users/OtherUser.js
@@ -24,61 +24,10 @@ Router.get('/:userName' , async (req, res) => {
     }
 })
 
-Router.post('/update', async (req, res) => {
+Router.get('/update', async (req, res) => {
     // const { firstName, lastName, description, heading, gender, dob, education } = req.body;
-    const { firstName } = req.body;
-
-    if(firstName) {
         return res.json({exist: `Got a UserId ${firstName}`});
-    }
 
-
-    // try {
-    //     let updateData = {
-    //         firstName,
-    //         lastName,
-    //         dob,
-    //         gender,
-    //         education,
-    //         "UserProfile.description": description,
-    //         "UserProfile.heading": heading,
-    //     };
-
-    //     if (req.file) {
-    //         const uploadResult = await cloudinary.uploader.upload(req.file.path, {
-    //             folder: "studyverse/profiles",
-    //         });
-
-    //         updateData["UserProfile.avatar"] = {
-    //             url: uploadResult.secure_url,
-    //             publicId: uploadResult.public_id,
-    //         };
-    //     }
-
-    //     const updatedProfile = await User.findOneAndUpdate(
-    //         { firebaseUid: userId },
-    //         updateData,
-    //         { new: true }
-    //     );
-
-    //     if (!updatedProfile) {
-    //         return res.status(404).json({ message: "User not found", code: "USER_NOT_FOUND" });
-    //     }
-
-    //     res.json({ 
-    //         ok: true, 
-    //         message: "Profile updated successfully", 
-    //         user: updatedProfile 
-    //     });
-
-    // } catch (err) {
-    //     console.error("Error updating profile:", err);
-    //     res.status(500).json({ 
-    //         ok: false,
-    //         message: "Internal server error", 
-    //         code: "INTERNAL_SERVER_ERROR" 
-    //     });
-    // }
 });
 
 module.exports = Router
