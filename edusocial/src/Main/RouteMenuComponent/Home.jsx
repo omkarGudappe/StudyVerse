63f30a38757420d427a793b21ccbe441c1b89508
omkarGudappe <omkar.gudappe205@gmail.com>
2025-09-13 12:18:47 +0530

    added AI features

--- edusocial/src/Main/RouteMenuComponent/Home.jsx
+++ edusocial/src/Main/RouteMenuComponent/Home.jsx
@@ -281,15 +281,18 @@ observer.current = new IntersectionObserver(
   return (
     <div className="lenis min-h-screen bg-gradient-to-br from-neutral-900 to-neutral-800 text-white py-8">
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
-        {/* Header Section */}
-        <div className="text-center mb-12">
-          <div className="inline-flex items-center justify-center w-20 h-20 bg-gradient-to-br from-purple-600 to-amber-500 rounded-2xl mb-6 shadow-lg">
-            {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-            </svg> */}
-            <img src='/LOGO/StudyVerseIcon.png' alt="" />
-          </div>
-          <h1 className="text-4xl md:text-5xl font-bold bg-gradient-to-r from-purple-400 to-amber-400 bg-clip-text text-transparent mb-4">
+        <div className='md:hidden'>
+          <nav className='fixed z-30 gap-0 top-0 left-0 w-full h-15'>
+            <div className='h-20 backdrop-blur-sm overflow-hidden flex items-center'>
+              <img src="/LOGO/StudyVerseLogo2.png" className='h-30 object-cover' alt="" />
+            </div>
+            <div className='py-1 px-1'>
+              <input type="text" className='bg-neutral-800 p-2 px-4 w-full rounded-2xl placeholder:text-white text-white outline-none' placeholder='Enter data' />
+            </div>
+          </nav>
+        </div>
+        <div className="text-center mb-12 md:mt-0 mt-25">
+          <h1 className="text-4xl mt-5 md:mt-0 md:text-5xl font-bold bg-gradient-to-r from-purple-400 to-amber-400 bg-clip-text text-transparent mb-4">
             StudyVerse Community
           </h1>
           <p className="text-lg text-neutral-300 max-w-2xl mx-auto">
