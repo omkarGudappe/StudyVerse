f8bd748a65e887ff5657954478bc1f53938627f0
omkarGudappe <omkar.gudappe205@gmail.com>
2026-06-04 18:44:10 +0530

    Update PDFViewer

--- edusocial/src/Main/RouteMenuComponent/NotesPDF.jsx
+++ edusocial/src/Main/RouteMenuComponent/NotesPDF.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useParams, useLocation } from 'react-router-dom';
-import PdfViewer from './SmallComponents/PDFViewer';
+import PdfViewer from './SmallComponents/PdfViewer';
 import axios from "axios";
 
 const NotesPDF = () => {
